34b0744e82da354a8cafffe2f345a5ed52cbe4d5
mohsin-8 <asif_khan1970@hotmail.com>
2021-04-16 01:07:00 +0500

    Practice on View Components

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
+      <Text>Hello World Mohsin</Text>
       <StatusBar style="auto" />
     </View>
   );
